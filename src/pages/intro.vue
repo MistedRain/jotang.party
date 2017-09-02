3a5c36aeff5846fffc946a0f03d352149672b07e
Yongcong Lin <646677472@qq.com>
2017-09-02 20:27:40 +0800

    加入简介

--- src/pages/intro.vue
+++ src/pages/intro.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrapper">
     <jotang-logo/>
-    <h1>简介区域</h1>
+    <h1>焦糖（ JoTang ）工作室目前位于电子科大沙河校区三教507，是一个倡导以工程师文化、极客文化为核心的 JSM 精神的工作室。</h1>
+    <h2>Just do it：即想即做，敢于大胆尝试</h2>
+    <h2>Share new imports：善于总结，喜欢分享</h2>
+    <h2>Make things beautiful：享受捣鼓，爱折腾，匠人之心</h2>
   </div>
 </template>
 
@@ -15,5 +18,13 @@ export default {
 </script>
 
 <style lang="css">
-
+h1,h2{
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  vertical-align: center;
+  color: #1F2D3D;
+  margin-top: 40px;
+}
 </style>
--- src/pages/jotangers.vue
+++ src/pages/jotangers.vue
@@ -1,71 +1,17 @@
 <template>
   <div class="wrapper">
     <mu-card>
-      <mu-card-media title="沈馨悦" subTitle="Image Sub Title">
-        <img src="/static/images/sxy.jpg" width="200" height="400" />
-      </mu-card-media>
-      <mu-card-title title="沈馨悦" subTitle="电子科技大学 2015级 网络安全专业 焦糖工作室负责人之一" />
+      <mu-card-header title="Myron Avatar" subTitle="sub title">
+        <mu-avatar src="../../static/images/wht.jpg" slot="avatar" />
+      </mu-card-header>
+      <mu-card-title title="Content Title" subTitle="Content Title" />
       <mu-card-text>
-        <br/>研究方向： 安全数据科学
-        <br/>赛事经历：
-        <br/>2016年全国大学生智能互联创新大赛西华赛区一等奖、全国三等奖
-        <br/>“花旗杯”四川省决赛
-        <br/>“IBM大型主机”大赛
-        <br/>“互联网+”西财赛区一等奖
-        <br/>微软创新杯 省级优胜奖
-        <br/>SAP智能互联创新大赛 一等奖+最佳创意奖
-        <br/>四川省计算机作品大赛
-        <br/>作品 《OWASP Cheatsheet Book》译者之一
+        散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。 调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。 似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光， 找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
       </mu-card-text>
-    </mu-card>
-    <mu-card>
-      <mu-card-media title="唐雷雨" subTitle="Image Sub Title">
-        <img src="/static/images/tly.jpg" height="400" />
-      </mu-card-media>
-      <mu-card-title title="唐雷雨" subTitle="电子科技大学 2015级 信息与软件技术专业 焦糖工作室负责人" />
-      <mu-card-text>
-        <br/>研究方向： web前端、机器学习
-        <br/>项目经历
-        <br/>两次中央高校项目--Moodfeeler、多平台应用开发及软文化人文影响力研究
-        <br/>赛事经历
-        <br/>微软创新杯 省级优胜奖
-        <br/>SAP智能互联创新大赛 一等奖+最佳创意奖
-        <br/>四川省计算机作品大赛省二等奖 “华为杯”
-        <br/>中国大学生智能设计大赛全国一等奖
-        <br/>“互联网+” 铜奖
-      </mu-card-text>
-    </mu-card>
-    <mu-card>
-      <mu-card-media title="吴昊天" subTitle="Image Sub Title">
-        <img src="/static/images/wht.png" height="400" />
-      </mu-card-media>
-      <mu-card-title title="吴昊天" subTitle="电子科技大学 2015级 信息与软件技术专业 焦糖工作室负责人" />
-      <mu-card-text>
-        <br/>初中开始编程、项目开发经验不计其数并且开发了 Python 社区 Star 数最多的微信公众号开发框架 WeRoBot 的大佬
-        <br/>赛事经历：
-        <br/>微软创新杯 省级优胜奖
-        <br/>SAP智能互联创新大赛 一等奖+最佳创意奖
-        <br/>四川省计算机作品大赛省二等奖
-        <br/>“互联网+” 铜奖
-        <br/>关于昊天大佬的更多秘密仍在挖掘中
-      </mu-card-text>
-    </mu-card>
-    <mu-card>
-      <mu-card-media title="范翥峰" subTitle="Image Sub Title">
-        <img src="/static/images/fzf.jpg" height="400" />
-      </mu-card-media>
-      <mu-card-title title="范翥峰" subTitle="电子科技大学 2015级 嵌入式系统专业 焦糖工作室负责人" />
-      <mu-card-text>
-        <br/>研究方向： Web全栈 Linux下C编程与后端
-        <br/>项目经历：
-        <br/>寻-脱离GPS的离线Web室内导航应用
-        <br/>Moodfeeler
-        <br/>赛事经历：
-        <br/>微软创新杯 省级优胜奖
-        <br/>SAP智能互联创新大赛 一等奖+最佳创意奖
-        <br/>四川省计算机作品大赛
-        <br/>“互联网+” 铜奖
-      </mu-card-text>
+      <mu-card-actions>
+        <mu-flat-button label="Action 1" />
+        <mu-flat-button label="Action 2" />
+      </mu-card-actions>
     </mu-card>
 
   </div>

4af3e15b12ded94fb336c5b45652a7ee1aa5ffec
Yongcong Lin <646677472@qq.com>
2017-09-03 17:38:03 +0800

    更换头像和图标

--- src/pages/jotangers.vue
+++ src/pages/jotangers.vue
@@ -1,59 +1,59 @@
 <template>
   <div class="wrapper">
     <mu-card>
-      <mu-card-header title="Myron Avatar" subTitle="sub title">
+      <mu-card-header title="吴昊天" subTitle="whtsky">
         <mu-avatar src="../../static/images/wht.jpg" slot="avatar" />
       </mu-card-header>
       <mu-card-title title="Content Title" subTitle="Content Title" />
       <mu-card-text>
-        散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。 调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。 似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光， 找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
+        2333
       </mu-card-text>
       <mu-card-actions>
-        <mu-flat-button label="Action 1" />
-        <mu-flat-button label="Action 2" />
+        <mu-flat-button icon=":icon-custom-github" label="Github" />
+        <mu-flat-button icon="person_outline" label="Blog" />
       </mu-card-actions>
     </mu-card>
-        <mu-card>
-      <mu-card-header title="Myron Avatar" subTitle="sub title">
-        <mu-avatar src="../../static/images/wht.jpg" slot="avatar" />
+    <br>
+    <mu-card>
+      <mu-card-header title="沈馨悦" subTitle="zuozuovera">
+        <mu-avatar src="../../static/images/sxy.jpg" slot="avatar" />
       </mu-card-header>
       <mu-card-title title="Content Title" subTitle="Content Title" />
       <mu-card-text>
-        散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。 调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。 似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光， 找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
+        2333
       </mu-card-text>
       <mu-card-actions>
-        <mu-flat-button label="Action 1" />
-        <mu-flat-button label="Action 2" />
+        <mu-flat-button icon=":icon-custom-github" label="Github" />
+        <mu-flat-button icon="person_outline" label="Blog" />
       </mu-card-actions>
     </mu-card>
-        <mu-card>
-      <mu-card-header title="Myron Avatar" subTitle="sub title">
-        <mu-avatar src="../../static/images/wht.jpg" slot="avatar" />
+    <br>
+    <mu-card>
+      <mu-card-header title="唐雷雨" subTitle="lilytly123">
+        <mu-avatar src="../../static/images/tly.jpg" slot="avatar" />
       </mu-card-header>
       <mu-card-title title="Content Title" subTitle="Content Title" />
       <mu-card-text>
-        散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。 调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。 似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光， 找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
+        2333
       </mu-card-text>
       <mu-card-actions>
-        <mu-flat-button label="Action 1" />
-        <mu-flat-button label="Action 2" />
+        <mu-flat-button icon=":icon-custom-github" label="Github" />
+        <mu-flat-button icon="person_outline" label="Blog" />
       </mu-card-actions>
     </mu-card>
-    
-  <mu-card>
-    <mu-card-header title="Myron Avatar" subTitle="sub title">
-      <mu-avatar src="../../static/images/wht.jpg" slot="avatar" />
-    </mu-card-header>
-    <mu-card-title title="Content Title" subTitle="Content Title" />
-    <mu-card-text>
-      散落在指尖的阳光，我试着轻轻抓住光影的踪迹，它却在眉宇间投下一片淡淡的阴影。 调皮的阳光掀动了四月的心帘，温暖如约的歌声渐起。 似乎在诉说着，我也可以在漆黑的角落里，找到阴影背后的阳光， 找到阳光与阴影奏出和谐的旋律。我要用一颗敏感赤诚的心迎接每一缕滑过指尖的阳光！
-    </mu-card-text>
-    <mu-card-actions>
-      <mu-flat-button label="Action 1" />
-      <mu-flat-button label="Action 2" />
-    </mu-card-actions>
-  </mu-card>
-
-
+    <br>
+    <mu-card>
+      <mu-card-header title="范翥峰" subTitle="fanzhuhhh">
+        <mu-avatar src="../../static/images/fzf.jpg" slot="avatar" />
+      </mu-card-header>
+      <mu-card-title title="Content Title" subTitle="Content Title" />
+      <mu-card-text>
+        2333
+      </mu-card-text>
+      <mu-card-actions>
+        <mu-flat-button icon=":icon-custom-github" label="Github" />
+        <mu-flat-button icon="person_outline" label="Blog" />
+      </mu-card-actions>
+    </mu-card>
   </div>
 </template>
